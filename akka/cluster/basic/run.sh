f56fa1b83e19e209701bc5abafa869aa3eddef0a
amutake <amutake.s@gmail.com>
2014-07-17 19:09:07 +0900

    Add cluster example

---
+++ akka/cluster/basic/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CLUSTER_IP=127.0.0.1
+export CLUSTER_PORT=$1
+if [ "$2" != "" ]; then
+    a=akka.tcp://cluster@127.0.0.1:$2
+fi
+if [ "$3" != "" ]; then
+    b=akka.tcp://cluster@127.0.0.1:$3
+fi
+
+
+sbt "run $a $b"
